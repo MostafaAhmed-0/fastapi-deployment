7ed72c86404c306bfe2f1278fb12065e2474796f
Mostafa Ahmed <asdfasdf44334433f@gmail.com>
2026-03-02 22:59:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -198,4 +198,6 @@ async def websocket_video_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
